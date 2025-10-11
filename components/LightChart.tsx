266d3209cce450f67532c42109fa830048432a7e
Scott <dkudviklerspg@gmail.com>
2025-10-12 00:20:04 +0200

    df

--- components/LightChart.tsx
+++ components/LightChart.tsx
@@ -46,7 +46,7 @@ export default function LightChart({ data, height = 400, width, loading = false 
     })
 
     // Create line series
-    const lineSeries = chart.addLineSeries({
+    const lineSeries = chart.addSeries('Line', {
       color: '#3b82f6',
       lineWidth: 2,
       crosshairMarkerVisible: true,
